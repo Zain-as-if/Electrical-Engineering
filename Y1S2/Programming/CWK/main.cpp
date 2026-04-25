78bdfa0f1a4832d785b09ab58017db75f4cacc37
Zain As If <zainasif154@gmail.com>
2026-04-25 22:15:40 +0100

    Finished bubble sort code

--- Y1S2/Programming/CWK/main.cpp
+++ Y1S2/Programming/CWK/main.cpp
@@ -3,34 +3,136 @@
 
 using namespace std;
 
+void bubbleSort(double *numberArray, int size);
+double calculateMean(double *numberArray, int size);
+double calculateMode(double *numberArray, int size);
+double calculateMedian(double *numberArray, int size);
+
 int main(void) {
   // initialise variables
-  fstream myFile;
-  char filename[20];
+  char inputFilename[20];
+  char exportFilename[20];
   const int MAX_SIZE = 10000;
-  int numberArray[MAX_SIZE];
+  double numberArray[MAX_SIZE];
+  // number list information
   int size = 0;
-  int i, j;
+  double minimum, maximum, mean, mode, median;
 
   // user input for filename
   cout << "Enter filename: ";
-  cin >> filename;
+  cin >> inputFilename;
 
+  fstream inputFile;
   // Opens file with name provided, read-only
-  myFile.open(filename, ios::in);
+  inputFile.open(inputFilename, ios::in);
 
   // error handling
-  if (!myFile) {
+  if (!inputFile) {
     cout << "Error: Could not open file.\n";
     return 1;
   }
 
   // copys numbers from file to array while counting the size
   // due to larger array initialised than needed as no dynamic arrays in c++
-  while (myFile >> numberArray[size] && size < MAX_SIZE) {
+  while (inputFile >> numberArray[size] && size < MAX_SIZE) {
     size++;
   }
 
+  inputFile.close();
+
+  // call bubbleSort function
+  bubbleSort(numberArray, size);
+  mean = calculateMean(numberArray, size);
+  median = calculateMedian(numberArray, size);
+  mode = calculateMode(numberArray, size);
+
+  cout << "--- Info ---" << endl;
+  cout << "Length: " << size << endl;
+  cout << "Mean: " << mean << endl;
+  cout << "Median: " << median << endl;
+  if (mode == -1) {
+    cout << "Mode: N/A" << endl;
+  } else {
+    cout << "Mode: " << mode << endl;
+  }
+  cout << "Minimum: " << numberArray[0] << endl;
+  cout << "Maximum: " << numberArray[size-1] << endl;
+
+  cout << endl << "Enter the name of the file you want the sorted data: ";
+  cin >> exportFilename;
+
+  fstream exportFile;
+
+  exportFile.open(exportFilename, ios::out);
+
+  for (int i = 0; i < size; i++) {
+    exportFile << numberArray[i];
+    exportFile << " ";
+  }
+
+  exportFile.close();
+
+  cout << "File: " << exportFilename << " has been written to.";
+
   cout << endl;
   return 0;
+}
+
+void bubbleSort(double *numberArray, int size) {
+  for (int i = 0; i < size - 1; i++) {
+    double *ptr = numberArray;
+
+    for (int j = 0; j < size - i - 1; j++) {
+      if (*ptr > *(ptr + 1)) {
+        double temp = *ptr;
+        *ptr = *(ptr + 1);
+        *(ptr + 1) = temp;
+      }
+
+      ptr++;
+    }
+  }
+}
+
+double calculateMean(double *numberArray, int size) {
+  double sum = 0;
+  for (int i = 0; i < size; i++) {
+    sum += numberArray[i]; 
+  }
+
+  return sum / size;
+}
+
+double calculateMode(double *numberArray, int size) {
+  int maxCount = 1;
+  int count = 1;
+  double mode = numberArray[0];
+
+  for (int i = 1; i < size; i++) {
+    if (numberArray[i] == numberArray[i-1]) {
+      count++;
+    } else {
+      count = 1;
+    }   
+    
+    if (count >= maxCount) {
+      maxCount = count;
+      mode = numberArray[i];
+    }
+  }
+
+  // if there is no mode set mode to be -1
+  if (maxCount == 1) {
+    mode = -1;
+  }
+
+  return mode;
+}
+
+double calculateMedian(double *numberArray, int size) {
+  if (size % 2 == 0) {
+    return ((numberArray[(size/2) - 1] + numberArray[size/2]) / 2.0);
+  } else {
+    return (numberArray[size/2]);
+  }
 }
